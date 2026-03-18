38c5abe13a6a0f20bc6f4971cfefb2ff019568e7
benaytms <benaytomas@gmail.com>
2026-03-18 20:17:26 -0300

    second script started

---
+++ index.py
@@ -0,0 +1,5 @@
+from main import get_movies
+
+if __name__ == "__main__":
+    movies = get_movies('ye4rz3r0')
+    print(movies[0])
--- main.py
+++ main.py
@@ -10,19 +10,35 @@ import os
 
 CACHE_FILE="db/watchlists.json"
 
-headers={"User-Agent": "Mozilla/5.0"}
+
 
 def load_cache():
+    """
+        If the JSON file with all watchlists already exists, load it
+        If not, then just starts a new one
+    """
     if os.path.exists(CACHE_FILE):
         with open(CACHE_FILE, "r") as f:
             return json.load(f)
     return {}
 
+
+
 def save_cache(cache):
+    """
+        Saves the cache to the JSON file
+    """
     with open(CACHE_FILE, "w") as f:
         json.dump(cache, f, indent=2)
 
-def parse_films(USER:str)->list:
+
+
+def parse_films(USER:str)->list[dict]:
+    """ 
+        Parse through the given user watchlist and saves all their movies
+        into a list
+    """
+    headers={"User-Agent": "Mozilla/5.0"}
     movies = []
     page = 1
     while True:
@@ -62,24 +78,14 @@ def parse_films(USER:str)->list:
         time.sleep(1)
     return movies
 
-def select_random(movies:list, USER:str)->list[dict]:
-    num_movies=len(movies)
-    n = int(input("How many to select: "))
-    if (not isinstance(n, int)) or n<0 or n>num_movies:
-        click.echo("Invalid parameter")
-        sys.exit(0)
-    return sample(movies, n)
 
-@click.command()
-@click.option('--user', default=None, help='Letterboxd Username')
-def main(user:str)->None:
-    """ Parses through USER letterboxd watchlist and select a sample of movies """
-    USER=user
-    if USER is None:
-        click.echo("No value provided\n")
-        click.echo('Run "python [script] --help" for more information')
-        sys.exit(0)
 
+def get_movies(USER:str)->list[dict]:
+    """
+        If the user already has an entry in the JSON file returns
+        that user movie watchlist. If not,
+        Uses the parse_films function to get the films
+    """
     cache=load_cache()
     if USER in cache:
         movies = cache[USER]
@@ -87,14 +93,49 @@ def main(user:str)->None:
         movies = parse_films(USER)
         cache[USER] = movies
         save_cache(cache)
+    return movies
 
-    selected_movies=select_random(movies, USER)
-    print("Selected movies were: \n") 
-    for idx in range(len(selected_movies)):
-        title,year = (selected_movies[idx]['title'], selected_movies[idx]['year'])
-        print(f"{title} - {year}")
-        time.sleep(1)
+
+
+def select_random(movies:list)->list[dict]|None:
+    """
+        Selects a sample of the user watchlist. Size of sample needs to be specified.
+    """
+    num_movies=len(movies)
+    n = int(input("How many to select: "))
+    if n==0:
+        return None
+    if (not isinstance(n, int)) or n<0 or n>num_movies:
+        click.echo("Invalid parameter")
+        sys.exit(0)
+    return sample(movies, n)
+
+
+
+@click.command()
+@click.option('--user', default=None, help='Letterboxd Username')
+def integrate(user:str)->None:
+    """ 
+        Parses through USER letterboxd watchlist and select a sample of movies.
+    """
+    USER=user
+    if USER is None:
+        click.echo("No value provided\n")
+        click.echo('Run "python [script] --help" for more information')
+        sys.exit(0)
+
+    movies=get_movies(USER)
+    selected_movies=select_random(movies)
+
+    if selected_movies is None:
+        click.echo("No movie selected.")
+    else:
+        print("Selected movies were: \n") 
+        for idx in range(len(selected_movies)):
+            title,year = (selected_movies[idx]['title'], selected_movies[idx]['year'])
+            print(f"{title} - {year}")
+            time.sleep(1)
 
 
 if __name__ == "__main__":
-    main()
+    integrate()
